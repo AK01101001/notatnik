4ca8da93cb879751ace554ee0c6e575b785d3f50
AK01101001 <147595111+AK01101001@users.noreply.github.com>
2025-01-29 16:16:20 +0100

    Add files via upload

---
+++ InfoAutor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace notatnik_
+{
+    /// <summary>
+    /// Logika interakcji dla klasy InfoAutor.xaml
+    /// </summary>
+    public partial class InfoAutor : Window
+    {
+        public InfoAutor()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace notatnik_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string nazwa { get; set; } = "";
+        bool wprowadzonoZmiany { get; set; } =false;
+        bool motywNocny {  get; set; } =false;
+        public string textSize { get; set; } = "10";
+        Brush buttonfore = Brushes.Black;
+        Brush buttonback = Brushes.White;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void NewFile(object sender, RoutedEventArgs e)
+        {
+            if (UnsavedContentWarning())
+            {
+                nazwa = "";
+                miejsce.Text = "";
+            }
+            UpdateWindowName();
+        }
+        
+        private void Zmiana(object sender, RoutedEventArgs e)
+        {
+            Title = nazwa+ "*";
+            wprowadzonoZmiany = true;
+            
+        }
+        private void textSizeChange(object sender, RoutedEventArgs e)
+        {
+            textSize = textSizeInput.Text;
+            if (int.TryParse(textSize,out int size))
+            {
+                if (size>10&&size<1000)
+                {
+                    miejsce.FontSize = size;
+                }
+            }
+        }
+        private void UpdateWindowName()
+        {
+            Title = nazwa;
+            wprowadzonoZmiany=false;
+        }
+
+        bool UnsavedContentWarning()
+        {
+            if (miejsce.Text==""||!wprowadzonoZmiany)
+            {
+                return true;
+            }
+            var result = MessageBox.Show("masz niezapisane zmainy czy chcesz je zapisac?", "Warning", MessageBoxButton.YesNoCancel);
+            if (result == MessageBoxResult.Yes)
+            {
+                File.WriteAllText(nazwa, miejsce.Text);
+            }
+            else if (result == MessageBoxResult.Cancel)
+            {
+                return false;
+            }
+            return true;
+        }
+        private void Save(object sender, RoutedEventArgs e)
+        {
+                        
+            if (nazwa == "")
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "text file | *.txt";
+                if (saveFileDialog.ShowDialog()==true)
+                {
+                    nazwa = saveFileDialog.FileName;
+                    File.WriteAllText(nazwa,miejsce.Text);
+                }
+                
+            }
+            else
+            {
+                File.WriteAllText(nazwa, miejsce.Text);
+            }
+            UpdateWindowName();
+        }
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+            if (UnsavedContentWarning())
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "text file | *.txt";
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    nazwa = openFileDialog.FileName;
+                    miejsce.Text = File.ReadAllText(openFileDialog.FileName);
+                }
+                UpdateWindowName();
+            }
+        }
+
+        private void Motyw(object sender, RoutedEventArgs e)
+        {
+            if (!motywNocny)
+            {
+                motywNocny = true;
+                miejsce.Foreground = Brushes.Gray;
+                miejsce.Background = Brushes.Black;
+                textSizeInput.Foreground = Brushes.Gray;
+                textSizeInput.Background = Brushes.Black;
+                window.Background = Brushes.Black;
+                trybButton.Background = Brushes.Black;
+                trybButton.Foreground = Brushes.White;
+                menu.Background = Brushes.Black;
+                menu.Foreground = Brushes.White; 
+                menu2.Background = Brushes.Black;
+                menu2.Foreground = Brushes.White;
+                buttonback = Brushes.Black;
+            }
+            else
+            {
+                motywNocny = false;
+                miejsce.Foreground = Brushes.Black;
+                miejsce.Background = Brushes.White;
+                textSizeInput.Foreground = Brushes.Black;
+                textSizeInput.Background = Brushes.White;
+                window.Background = Brushes.White;
+                trybButton.Background = Brushes.White;
+                trybButton.Foreground = Brushes.Black;
+                menu.Background = Brushes.White;
+                menu.Foreground = Brushes.Black;
+                menu2.Background = Brushes.White;
+                menu2.Foreground = Brushes.Black;
+            }
+        }
+
+        private void InfoApp(object sender, RoutedEventArgs e)
+        {
+            InfoApp infoApp = new InfoApp();
+            infoApp.Show();
+        }
+
+        private void InfoAutor(object sender, RoutedEventArgs e)
+        {
+            InfoAutor infoApp = new InfoAutor();
+            infoApp.ShowDialog();
+
+        }
+    }
+}
